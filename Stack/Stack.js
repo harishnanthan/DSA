4a1f20afdec631fae3fe08c9d480e56de427d769
harishnanthan <h.chetta17@gmail.com>
2023-06-12 12:27:52 +0530

    Stack

---
+++ Stack/Stack.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    }
+    this.first = temp.next;
+    --this.size;
+    return temp.value;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.pop();
+stack.pop();
+console.log(stack);
---
+++ Stack/Stack.md
@@ -0,0 +1,10 @@
+Stack
+  stack is the collection of data with the principle of LIFO, Last In First Out;
+
+Array is making stack with shift and un-shift methods
+
+BIG(o) of stacks;
+  Insertion O(1);
+  Deletion O(1);
+  Access O(n);     || for this you don't need a stack
+  Searching O(n);  || for this you don't need a stack
